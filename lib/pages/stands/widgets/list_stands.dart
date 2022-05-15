b831cd2c4cb85dee13355a8c9fd35c144ad7a017
NoelHuibers <Noel@huibers.io>
2022-05-15 16:23:23 +0200

    starLogic setState not proparly working

--- lib/pages/stands/widgets/list_stands.dart
+++ lib/pages/stands/widgets/list_stands.dart
@@ -11,13 +11,16 @@ class Standslist extends StatelessWidget {
       child: ListView.builder(
         itemCount: StandsModel.standsList.length,
         itemBuilder: (context, index) {
+          Widget _star = Star(starred: standsList[index]["starred"]);
           return ListTile(
-            leading: Star(starred: StandsModel.standsList[index]["starred"]),
-            title: Text(StandsModel.standsList[index]['title']),
-            subtitle: Text(StandsModel.standsList[index]['subtitle']),
+            leading: _star,
+            title: Text(standsList[index]['title']),
+            subtitle: Text(standsList[index]['subtitle']),
             onTap: () {
-              StandsModel.standsList[index]['starred'] =
-                  !StandsModel.standsList[index]['starred'];
+              standsList[index]['starred'] = !standsList[index]['starred'];
+              setState() {
+                _star = Star(starred: standsList[index]["starred"]);
+              }
             },
           );
         },
@@ -25,3 +28,17 @@ class Standslist extends StatelessWidget {
     );
   }
 }
+
+List standsList = [
+  {
+    "title": "Horizontbank",
+    "subtitle": "Info of Horizontbank",
+    "starred": true
+  },
+  {"title": "Weserstufen", "subtitle": "Info of Weserstufen", "starred": false},
+  {
+    "title": "Galeriegärten",
+    "subtitle": "Info of Galeriegärten",
+    "starred": false
+  }
+];
