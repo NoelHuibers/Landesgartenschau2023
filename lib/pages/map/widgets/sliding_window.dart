ce21de22ef1f1847d82069ed0e76fae36bf065a6
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2022-05-25 23:15:46 +0200

    sliding_window update

--- lib/pages/map/widgets/sliding_window.dart
+++ lib/pages/map/widgets/sliding_window.dart
@@ -1,140 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:landesgartenschau2023/pages/map/widgets/build_list.dart';
-import '../../../config/palette.dart';
-import '../../home/Button_bar.dart';
+import '../../home/button_bar.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-
-void flipFenster(context) {
-  showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(25.0),
-          bottom: Radius.circular(25.0),
-        ),
-      ),
-      builder: (BuildContext bc) {
-        return Center(
-            child: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(15),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          childAspectRatio: 1.09,
-          children: <Widget>[
-            InkWell(
-                onTap: () {
-                  eingabe_text(context).then((onValue) {
-                    print(onValue);
-                    //Hier kann man die adresse an Backend senden
-                  });
-                },
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Theme.of(context).colorScheme.primary),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.home,
-                            size: 50,
-                            color: Colors.red,
-                          ),
-                          Text(
-                            "Bring mich zurück",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ]))),
-            InkWell(
-                onTap: () {},
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Theme.of(context).colorScheme.primary),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.local_parking,
-                            size: 50,
-                            color: Colors.red,
-                          ),
-                          Text(
-                            "Parkplatz finden",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ]))),
-            InkWell(
-                onTap: () {},
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Theme.of(context).colorScheme.primary),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.forest_outlined,
-                            size: 50,
-                            color: Colors.red,
-                          ),
-                          Text(
-                            "Stände",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ]))),
-            InkWell(
-                onTap: () {},
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Theme.of(context).colorScheme.primary),
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Icon(
-                            Icons.wc,
-                            size: 50,
-                            color: Colors.red,
-                          ),
-                          Text(
-                            "WC finden",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ]))),
-          ],
-        ));
-      });
-}
-
-Future<String?> eingabe_text(BuildContext context) {
-  TextEditingController controller = TextEditingController();
-
-  return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Startadresse"),
-          content: TextField(
-            decoration:
-                const InputDecoration(hintText: "Startadresse eingeben"),
-            autofocus: true,
-            controller: controller,
-          ),
-          actions: [
-            MaterialButton(
-              elevation: 5.0,
-              child: const Text("eigeben"),
-              onPressed: () {
-                Navigator.of(context).pop(controller.text.toString());
-              },
-            )
-          ],
-        );
-      });
-}
 
 class slide_page extends StatelessWidget {
   final ScrollController controller;
@@ -152,7 +19,7 @@ class slide_page extends StatelessWidget {
       controller: controller,
       children: <Widget>[
         SizedBox(height: 20, child: swipe_bottum()),
-        const SizedBox(height: 80, child: bottom_bar()),
+        SizedBox(height: 80, child: bottom_bar()),
         SizedBox(
           height: 500,
           child: buildList(),
