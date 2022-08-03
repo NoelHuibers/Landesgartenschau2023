abe742d4dbe079819fc8617889d9fd17aff9b0b4
SimonOdischo <103313046+SimonOdischo@users.noreply.github.com>
2022-08-03 14:41:09 +0200

    showAboutDialog update

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -2,16 +2,28 @@ import 'package:flutter/material.dart';
 
 mixin Palette {
   static final ThemeData darkTheme = ThemeData(
+    listTileTheme: const ListTileThemeData(textColor: Colors.white),
+    cardColor: const Color.fromARGB(255, 28, 28, 28),
     scaffoldBackgroundColor: const Color.fromARGB(6, 179, 179, 179),
     appBarTheme: const AppBarTheme(
-      color: Color.fromARGB(6, 94, 4, 4),
+      // ignore: deprecated_member_use
+      textTheme: TextTheme(
+        subtitle2: TextStyle(color: Colors.white, fontSize: 15),
+      ),
+      color: Color(0xE6333333),
+      titleTextStyle:
+          TextStyle(color: Color.fromARGB(255, 234, 234, 234), fontSize: 25),
       iconTheme: IconThemeData(
-        color: Color.fromARGB(255, 28, 28, 28),
+        color: Color.fromARGB(255, 234, 234, 234),
       ),
     ),
-
-    primarySwatch: Colors.red,
-
+    dialogTheme: const DialogTheme(
+      backgroundColor: Color(0xE6333333),
+    ),
+    textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+            foregroundColor:
+                MaterialStateProperty.resolveWith((state) => Colors.white))),
     colorScheme: const ColorScheme.dark(
         primary: Color.fromARGB(255, 28, 28, 28), // dark=grau - light=grau
         onPrimary: Color(0xFFF6FDFF), // dark=weiß - light=grau
@@ -19,48 +31,57 @@ mixin Palette {
         surfaceVariant: Color(0xE6333333),
         surfaceTint: Color(0xE6333333) // locationmarker
         ),
-
     iconTheme: const IconThemeData(
       color: Color(0xFFF6FDFF),
     ),
-    // Link Text Color: 1E77B7
-
     textTheme: const TextTheme(
       headline5: TextStyle(
         color: Colors.white,
-        //fontSize: 20.0,
       ), // Überschrift (appbar)
       subtitle1: TextStyle(
         color: Color(0xE6333333),
-        //fontSize: 18.0,
       ), // Switch Labeltext
       subtitle2: TextStyle(
         color: Color(0xE6333333),
-        //fontSize: 18.0,
       ), // Sliding Panel
       labelMedium: TextStyle(
         color: Color(0xE6333333),
-        //fontSize: 16.0,
       ), // Sliding Panel
       bodyText1: TextStyle(
         color: Color(0xFFF6FDFF),
-        //fontSize: 14.0,
       ), // Haupttext2
       bodyText2: TextStyle(
         color: Color.fromARGB(255, 255, 255, 255),
-        //fontSize: 14.0,
-      ), // Haupttext1
+      ),
+      caption: TextStyle(
+        color: Colors.white,
+      ),
     ),
   );
 
   static final ThemeData lightTheme = ThemeData(
+    listTileTheme: const ListTileThemeData(textColor: Colors.black),
     scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
+    cardColor: const Color.fromARGB(255, 255, 255, 255),
     appBarTheme: const AppBarTheme(
-      color: Color.fromARGB(197, 187, 187, 187),
+      // ignore: deprecated_member_use
+      textTheme: TextTheme(
+        subtitle2: TextStyle(color: Colors.black, fontSize: 15),
+      ),
+      color: Color.fromARGB(230, 199, 195, 195),
+      titleTextStyle:
+          TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 25),
       iconTheme: IconThemeData(
-        color: Color.fromARGB(255, 255, 255, 255),
+        color: Color.fromARGB(255, 0, 0, 0), //AppBar Button
       ),
     ),
+    dialogTheme: const DialogTheme(
+      backgroundColor: Color.fromARGB(239, 213, 212, 212),
+    ),
+    textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+            foregroundColor:
+                MaterialStateProperty.resolveWith((state) => Colors.black))),
     primarySwatch: Colors.blue,
     colorScheme: const ColorScheme.light(
       primary: Color.fromARGB(255, 255, 255, 255), // dark=grau - light=grau
@@ -75,28 +96,25 @@ mixin Palette {
     textTheme: const TextTheme(
       headline5: TextStyle(
         color: Colors.black,
-        //fontSize: 20.0,
       ), // Überschrift (appbar)
       subtitle1: TextStyle(
         color: Colors.black,
-        //fontSize: 18.0,
       ), // Switch Labeltext
       subtitle2: TextStyle(
         color: Color(0xFFF6FDFF),
-        //fontSize: 18.0,
       ), // Sliding Panel
       labelMedium: TextStyle(
         color: Color(0xFFF6FDFF),
-        //fontSize: 16.0,
       ), // Sliding Panel
       bodyText1: TextStyle(
         color: Color(0xFFF6FDFF),
-        //fontSize: 14.0,
       ), // Haupttext2
       bodyText2: TextStyle(
         color: Color.fromARGB(230, 0, 0, 0),
-        //fontSize: 14.0,
-      ), // Haupttext
+      ),
+      caption: TextStyle(
+        color: Colors.black,
+      ),
     ),
   );
 }
